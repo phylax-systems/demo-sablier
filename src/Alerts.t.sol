1ae27c5fb89702072005f8c7cdb47e3fd4016183
andreivladbrg <andreivladbrg@gmail.com>
2024-01-17 21:23:34 +0200

    refactor: change snake_case to camelCase

--- src/Alerts.t.sol
+++ src/Alerts.t.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.8.23;
 import { Alert } from "phylax-std/src/Alert.sol";
 import { console2 } from "forge-std/console2.sol";
 
-import { Mainnet_Addresses } from "./MainnetAddresses.t.sol";
+import { MainnetAddresses } from "./MainnetAddresses.t.sol";
 
-abstract contract Base_Sablier_Alert is Alert {
+abstract contract BaseSablierAlert is Alert {
     address _dai;
     address _usdc;
     address _usdt;
@@ -17,74 +17,73 @@ abstract contract Base_Sablier_Alert is Alert {
         _usdt = usdt_;
     }
 
-    function test_balance_alert(address token, address sablier_contract) internal {
-        uint256 current_balance = IERC20Like(token).balanceOf(sablier_contract);
+    function test_BalanceAlert(address token, address sablierContract) internal {
+        uint256 currentBalance = IERC20Like(token).balanceOf(sablierContract);
 
         // roll back 5 blocks
         vm.rollFork({ blockNumber: block.number - 5 });
 
-        uint256 previous_balance = IERC20Like(token).balanceOf(sablier_contract);
+        uint256 previousBalance = IERC20Like(token).balanceOf(sablierContract);
 
         // check if the previous balance is 30% less than the current balance
-        bool alert = current_balance * 70 / 100 > previous_balance;
+        bool alert = currentBalance * 70 / 100 > previousBalance;
 
-        string memory token_symbol = IERC20Like(token).symbol();
+        string memory tokenSymbol = IERC20Like(token).symbol();
 
-        string memory has_or_has_not = alert ? " has " : " has not ";
+        string memory hasOrHasNot = alert ? " has " : " has not ";
 
         string memory s =
-            string.concat("The balance of ", token_symbol, has_or_has_not, "decreased by 30% in the last 5 blocks");
+            string.concat("The balance of ", tokenSymbol, hasOrHasNot, "decreased by 30% in the last 5 blocks");
 
         console2.log(s);
 
         assertTrue(true);
     }
 
-    function test_stablecoin_alert(address token, address sablier_contract) internal {
-        uint256 current_stream_id = ISablierV2LockupLike(sablier_contract).nextStreamId();
+    function test_StablecoinAlert(address token, address sablierContract) internal {
+        uint256 currentStreamId = ISablierV2LockupLike(sablierContract).nextStreamId();
 
         // roll back 5 blocks
         vm.rollFork({ blockNumber: block.number - 5 });
 
-        uint256 previous_stream_id = ISablierV2LockupLike(sablier_contract).nextStreamId();
+        uint256 previousStreamId = ISablierV2LockupLike(sablierContract).nextStreamId();
 
-        uint256 deposited_sum;
+        uint256 depositedSum;
 
-        for (uint256 i = previous_stream_id; i < current_stream_id; ++i) {
-            address asset = ISablierV2LockupLike(sablier_contract).getAsset(i);
+        for (uint256 i = previousStreamId; i < currentStreamId; ++i) {
+            address asset = ISablierV2LockupLike(sablierContract).getAsset(i);
 
             if (asset == token) {
-                deposited_sum += ISablierV2LockupLike(sablier_contract).getDepositedAmount(i);
+                depositedSum += ISablierV2LockupLike(sablierContract).getDepositedAmount(i);
             }
         }
 
         uint8 decimals = IERC20Like(token).decimals();
 
         // check if there has been deposited more than 50k
-        bool alert = 10 ** decimals * 50_000 < deposited_sum;
+        bool alert = 10 ** decimals * 50_000 < depositedSum;
 
-        string memory token_symbol = IERC20Like(token).symbol();
-        string memory has_or_has_not = alert ? " has " : " has not ";
+        string memory tokenSymbol = IERC20Like(token).symbol();
+        string memory hasOrHasNot = alert ? " has " : " has not ";
 
-        string memory s = string.concat(
-            "There", has_or_has_not, "been deposited more than 50k ", token_symbol, " in the last 5 blocks"
-        );
+        string memory s =
+            string.concat("There", hasOrHasNot, "been deposited more than 50k ", tokenSymbol, " in the last 5 blocks");
 
         console2.log(s);
 
         assertTrue(true);
     }
 
-    function test_stablecoins_alert(address sablier_contract) internal {
-        test_stablecoin_alert(_dai, sablier_contract);
-        test_stablecoin_alert(_usdc, sablier_contract);
-        test_stablecoin_alert(_usdt, sablier_contract);
+    function test_StablecoinsAlert(address sablierContract) internal {
+        test_StablecoinAlert(_dai, sablierContract);
+        test_StablecoinAlert(_usdc, sablierContract);
+        test_StablecoinAlert(_usdt, sablierContract);
     }
 }
 
-contract Alerts_Mainnet is Mainnet_Addresses, Base_Sablier_Alert {
+contract AlertsMainnet is MainnetAddresses, BaseSablierAlert {
     constructor()
-        Base_Sablier_Alert(
+        BaseSablierAlert(
             0x6B175474E89094C44Da98b954EedeAC495271d0F,
             0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
             0xdAC17F958D2ee523a2206206994597C13D831ec7
@@ -97,36 +96,36 @@ contract Alerts_Mainnet is Mainnet_Addresses, Base_Sablier_Alert {
         mainnet = enableChain("mainnet");
     }
 
-    function test_tokens_balance_mainnet() public chain(mainnet) {
+    function test_TokensBalance_Mainnet() public chain(mainnet) {
         // v1.0 tokens
-        for (uint256 i; i < tokens_v10.length; ++i) {
-            test_balance_alert(tokens_v10[i], sablier_v10);
+        for (uint256 i; i < tokensV10.length; ++i) {
+            test_BalanceAlert(tokensV10[i], sablierV10);
         }
 
         // v1.1 tokens
-        for (uint256 i; i < tokens_v11.length; ++i) {
-            test_balance_alert(tokens_v11[i], sablier_v11);
+        for (uint256 i; i < tokensV11.length; ++i) {
+            test_BalanceAlert(tokensV11[i], sablierV11);
         }
 
         // v2.0 tokens
-        for (uint256 i; i < tokens_v20.length; ++i) {
-            test_balance_alert(tokens_v20[i], sablier_v20_linear);
+        for (uint256 i; i < tokensV20.length; ++i) {
+            test_BalanceAlert(tokensV20[i], sablierV20Linear);
         }
     }
 
-    function test_stablecoins_mainnet() public chain(mainnet) {
-        test_stablecoins_alert(sablier_v21_linear);
+    function test_Stablecoins_Mainnet() public chain(mainnet) {
+        test_StablecoinsAlert(sablierV21Linear);
     }
 }
 
-contract Alerts_Optimism is Base_Sablier_Alert {
+contract AlertsOptimism is BaseSablierAlert {
     address exa = 0x1e925De1c68ef83bD98eE3E130eF14a50309C01B;
 
-    address sablier_v20_linear = 0xB10daee1FCF62243aE27776D7a92D39dC8740f95;
-    address sablier_v21_linear = 0x4b45090152a5731b5bc71b5baF71E60e05B33867;
+    address sablierV20Linear = 0xB10daee1FCF62243aE27776D7a92D39dC8740f95;
+    address sablierV21Linear = 0x4b45090152a5731b5bc71b5baF71E60e05B33867;
 
     constructor()
-        Base_Sablier_Alert(
+        BaseSablierAlert(
             0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1,
             0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85,
             0x94b008aA00579c1307B0EF2c499aD98a8ce58e58
@@ -139,21 +138,21 @@ contract Alerts_Optimism is Base_Sablier_Alert {
         optimism = enableChain("optimism");
     }
 
-    function test_tokens_balance_optimism() public chain(optimism) {
-        test_balance_alert(exa, sablier_v20_linear);
+    function test_TokensBalance_Optimism() public chain(optimism) {
+        test_BalanceAlert(exa, sablierV20Linear);
     }
 
-    function test_stablecoins_optimism() public chain(optimism) {
-        test_stablecoins_alert(sablier_v21_linear);
+    function test_Stablecoins_Optimism() public chain(optimism) {
+        test_StablecoinsAlert(sablierV21Linear);
     }
 }
 
-contract Alerts_Arbitrum is Base_Sablier_Alert {
-    address sablier_v20_linear = 0xB10daee1FCF62243aE27776D7a92D39dC8740f95;
-    address sablier_v21_linear = 0xFDD9d122B451F549f48c4942c6fa6646D849e8C1;
+contract AlertsArbitrum is BaseSablierAlert {
+    address sablierV20Linear = 0xB10daee1FCF62243aE27776D7a92D39dC8740f95;
+    address sablierV21Linear = 0xFDD9d122B451F549f48c4942c6fa6646D849e8C1;
 
     constructor()
-        Base_Sablier_Alert(
+        BaseSablierAlert(
             0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1,
             0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9,
             0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9
@@ -166,8 +165,8 @@ contract Alerts_Arbitrum is Base_Sablier_Alert {
         arbitrum = enableChain("arbitrum");
     }
 
-    function test_stablecoins_arbitrum() public chain(arbitrum) {
-        test_stablecoins_alert(sablier_v21_linear);
+    function test_Stablecoins_Arbitrum() public chain(arbitrum) {
+        test_StablecoinsAlert(sablierV21Linear);
     }
 }
 
--- src/MainnetAddresses.t.sol
+++ src/MainnetAddresses.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.23;
 
-abstract contract Mainnet_Addresses {
+abstract contract MainnetAddresses {
     // v1.0 tokens
-    address[] tokens_v10;
+    address[] tokensV10;
     address fox = 0xc770EEfAd204B5180dF6a14Ee197D99d808ee52d;
     address dfx = 0x888888435FDe8e7d4c54cAb67f206e4199454c60;
     address dht = 0xca1207647Ff814039530D7d35df0e1Dd2e91Fa84;
@@ -11,7 +11,7 @@ abstract contract Mainnet_Addresses {
     address mpl = 0x33349B282065b0284d756F0577FB39c158F935e6;
 
     // v1.1 tokens
-    address[] tokens_v11;
+    address[] tokensV11;
     address flx = 0x3Ea8ea4237344C9931214796d9417Af1A1180770;
     address perc = 0x60bE1e1fE41c1370ADaF5d8e66f07Cf1C2Df2268;
     address spell = 0x090185f2135308BaD17527004364eBcC2D37e5F6;
@@ -26,41 +26,41 @@ abstract contract Mainnet_Addresses {
     address akita = 0x3301Ee63Fb29F863f2333Bd4466acb46CD8323E6;
 
     // v2.0 tokens
-    address[] tokens_v20;
+    address[] tokensV20;
     address duel = 0x943Af2ece93118B973c95c2F698EE9D15002e604;
     address dextf = 0x5F64Ab1544D28732F0A24F4713c2C8ec0dA089f0;
     address imx = 0xF57e7e7C23978C3cAEC3C3548E3D615c346e79fF;
     address tkst = 0x7CdBfC86A0BFa20F133748B0CF5cEa5b787b182c;
 
     // sablier contracts
-    address sablier_v10 = 0xA4fc358455Febe425536fd1878bE67FfDBDEC59a;
-    address sablier_v11 = 0xCD18eAa163733Da39c232722cBC4E8940b1D8888;
-    address sablier_v20_linear = 0xB10daee1FCF62243aE27776D7a92D39dC8740f95;
-    address sablier_v21_linear = 0xAFb979d9afAd1aD27C5eFf4E27226E3AB9e5dCC9;
+    address sablierV10 = 0xA4fc358455Febe425536fd1878bE67FfDBDEC59a;
+    address sablierV11 = 0xCD18eAa163733Da39c232722cBC4E8940b1D8888;
+    address sablierV20Linear = 0xB10daee1FCF62243aE27776D7a92D39dC8740f95;
+    address sablierV21Linear = 0xAFb979d9afAd1aD27C5eFf4E27226E3AB9e5dCC9;
 
     constructor() {
-        tokens_v10.push(fox);
-        tokens_v10.push(dfx);
-        tokens_v10.push(dht);
-        tokens_v10.push(occ);
-        tokens_v10.push(mpl);
+        tokensV10.push(fox);
+        tokensV10.push(dfx);
+        tokensV10.push(dht);
+        tokensV10.push(occ);
+        tokensV10.push(mpl);
 
-        tokens_v11.push(flx);
-        tokens_v11.push(perc);
-        tokens_v11.push(spell);
-        tokens_v11.push(fox);
-        tokens_v11.push(uwu);
-        tokens_v11.push(aleph);
-        tokens_v11.push(comp);
-        tokens_v11.push(usdc);
-        tokens_v11.push(occ);
-        tokens_v11.push(mpl);
-        tokens_v11.push(tru);
-        tokens_v11.push(akita);
+        tokensV11.push(flx);
+        tokensV11.push(perc);
+        tokensV11.push(spell);
+        tokensV11.push(fox);
+        tokensV11.push(uwu);
+        tokensV11.push(aleph);
+        tokensV11.push(comp);
+        tokensV11.push(usdc);
+        tokensV11.push(occ);
+        tokensV11.push(mpl);
+        tokensV11.push(tru);
+        tokensV11.push(akita);
 
-        tokens_v20.push(duel);
-        tokens_v20.push(dextf);
-        tokens_v20.push(imx);
-        tokens_v20.push(tkst);
+        tokensV20.push(duel);
+        tokensV20.push(dextf);
+        tokensV20.push(imx);
+        tokensV20.push(tkst);
     }
 }
